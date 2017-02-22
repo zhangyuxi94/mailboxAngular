94c4ebf9f86cdc7b897fc49936a96a22c0fc897f
zhangyuxi94 <zhang.yuxi1@husky.neu.edu>
2017-02-22 14:47:01 -0500

    updated send mail function

--- js/angularv1.js
+++ js/angularv1.js
@@ -200,6 +200,7 @@
             $scope.showMsgDetail=false;
             var userInfo=userData.userInfo;
             var thisUserId=$routeParams.id;
+            // console.log(findMailboxByUserid("inbox","S",userInfo,thisUserId));
             $scope.userid=thisUserId;
             var thisUserMsgs=userInfo[thisUserId-1].inbox;
             var thisUserSent=userInfo[thisUserId-1].sentmail;
@@ -279,6 +280,39 @@
                 var userDataStr=JSON.stringify(userInfo);
                 localStorage.removeItem("userdata");
                 localStorage.setItem("userdata",userDataStr);
+            };
+            // compose
+            $scope.usernameNotFind=false;
+            $scope.sendMail=function(mailReceiver,mailSubject,mailContent){
+                var receiverInfo=findUserByUsername(mailReceiver,userInfo);
+                var senderInfo=findUserById(thisUserId,userInfo);
+                if(receiverInfo===undefined){
+                    console.log(1);
+                    $scope.usernameNotFind=true;
+                }else{
+                    var thisTime=Date.now();
+                    var newMsg={
+                        "msgid":"msg"+thisTime,
+                        "recieverid":receiverInfo.userid,
+                        "reciever":receiverInfo.username,
+                        "senderid":thisUserId,
+                        "sender":senderInfo.username,
+                        "subject":mailSubject,
+                        "content":mailContent,
+                        "star":false
+                    };
+                    userInfo[thisUserId-1].sentmail.push(newMsg);
+                    userInfo[receiverInfo.userid-1].inbox.push(newMsg);
+                    updateLocalstorage(userInfo,"userdata");
+                    // console.log(userInfo);
+                }
+                // console.log(mailReceiver+" "+mailSubject+" "+mailContent)
+            }
+            $scope.cancelSendMail=function () {
+                $scope.usernameNotFind=false;
+                $scope.mailReceiver=undefined;
+                $scope.mailSubject=undefined;
+                $scope.mailContent=undefined;
             }
         }
         function findUserById(userid,userData){
@@ -287,6 +321,57 @@
                     return userData[i];
                 }
             }
+        }
+        function findUserByUsername(username,userData){
+            for(var i=0;i<userData.length;i++){
+                if(userData[i].username===username){
+                    return userData[i];
+                }
+            }
+        }
+        function updateLocalstorage(dataObject,lsName){
+            var dataStr=JSON.stringify(dataObject);
+            localStorage.removeItem(lsName);
+            localStorage.setItem(lsName,dataStr);
+        }
+        function findMailboxByUserid(whichBox,RorS,userData,userid){
+            var mailbox=[];
+            for(var i=0;i<userData.length;i++){
+                // find the sendbox of the sender
+                if(whichBox==="sentmail"&&RorS==="S"){
+                    for(var j=0;j<userData[i].sentmail.length;j++){
+                        if(userData[i].sentmail[j].senderid===userid){
+                            mailbox.push(userData[i].sentmail[j]);
+                        }
+                    }
+                }
+                // find the receivebox which has the sender's message
+                else if(whichBox==="inbox"&&RorS==="S"){
+                    for(var j1=0;j1<userData[i].inbox.length;j1++){
+                        if(userData[i].inbox[j1].senderid===userid){
+                            mailbox.push(userData[i].inbox[j1]);
+                        }
+                    }
+                }
+                // find others sendbox which has the sender's mail
+                else if(whichBox==="sentmail"&&RorS==="R"){
+                    for(var j2=0;j2<userData[i].sentmail.length;j2++){
+                        if(userData[i].sentmail[j2].recieverid===userid){
+                            console.log(userData[i]);
+                            mailbox.push(userData[i].sentmail[j2]);
+                        }
+                    }
+                }
+                // find the receivebox of the sender
+                else if(whichBox==="inbox"&&RorS==="R"){
+                    for(var j3=0;j3<userData[i].inbox.length;j3++){
+                        if(userData[i].inbox[j3].recieverid===userid){
+                            mailbox.push(userData[i].inbox[j3]);
+                        }
+                    }
+                }
+            }
+            return mailbox;
         }
     }
 })();
